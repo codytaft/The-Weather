7e8027b62ae7cff302c2e3bf618365cbd8958719
Cody Taft <cody.taft@gmail.com>
2018-11-13 12:12:59 -0700

    Change degrees to farenheit

--- src/Helpers/FetchCalls.js
+++ src/Helpers/FetchCalls.js
@@ -5,13 +5,13 @@ export const getFiveDayForecast = (city, zip) => {
   try {
     if (city) {
       return fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?APPID=${ApiKey}&q=${city}`
+        `http://api.openweathermap.org/data/2.5/forecast?APPID=${ApiKey}&q=${city}&units=imperial`
       )
         .then(response => response.json())
         .then(data => data.list);
     } else {
       return fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?APPID=${ApiKey}&zip=${zip}`
+        `http://api.openweathermap.org/data/2.5/forecast?APPID=${ApiKey}&zip=${zip}&units=imperial`
           .then(response => response.json())
           .then(data => data.list)
       );
